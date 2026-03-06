7d8e9fd99732a74df2b604cd1e88778e0662623b
peternmuller <pm563838@gmail.com>
2026-03-06 09:01:35 +0100

    Zigbee_Color_Dimmable_Light
    * From https://github.com/espressif/arduino-esp32/blob/master/libraries/Zigbee/examples/Zigbee_Color_Dimmable_Light/Zigbee_Color_Dimmable_Light.ino
    
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,105 +1,149 @@
-#include <SHT2x.h>
-#include <Wire.h>
-#include <Zigbee.h>
+// Copyright 2024 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @brief This example demonstrates Zigbee Color Dimmable light bulb with RGB and Temperature support.
+ *
+ * The example demonstrates how to use Zigbee library to create an end device with
+ * color dimmable light end point supporting both RGB (X/Y) and Color Temperature modes.
+ * The light bulb is a Zigbee end device, which is controlled by a Zigbee coordinator.
+ *
+ * Proper Zigbee mode must be selected in Tools->Zigbee mode
+ * and also the correct partition scheme must be selected in Tools->Partition Scheme.
+ *
+ * Please check the README.md for instructions and more detailed description.
+ *
+ * Created by Jan Procházka (https://github.com/P-R-O-C-H-Y/)
+ */
 
 #ifndef ZIGBEE_MODE_ED
 #error "Zigbee end device mode is not selected in Tools->Zigbee mode"
 #endif
-#define TEMP_SENSOR_ENDPOINT_NUMBER 10
-#define LED 15
 
-#define uS_TO_S_FACTOR 1000000ULL
-#define TIME_TO_SLEEP 60
-#define TEMP_THRESHOLD 1.0f
-#define HUM_THRESHOLD 2.0f
+#include <Zigbee.h>
 
-RTC_DATA_ATTR float derniereTemperature = -999.0f;
-RTC_DATA_ATTR float derniereHumidite = -999.0f;
+/* Zigbee color dimmable light configuration */
+#define ZIGBEE_RGB_LIGHT_ENDPOINT 10
+uint8_t led = RGB_BUILTIN;
+uint8_t button = BOOT_PIN;
 
-ZigbeeTempSensor zbTempSensor = ZigbeeTempSensor(TEMP_SENSOR_ENDPOINT_NUMBER);
-SHT2x sht;
+ZigbeeColorDimmableLight zbColorLight = ZigbeeColorDimmableLight(ZIGBEE_RGB_LIGHT_ENDPOINT);
 
-void setup() {
-  Serial.begin(115200);
-  Wire.begin();
-  sht.begin();
+/********************* Temperature conversion functions **************************/
+uint16_t kelvinToMireds(uint16_t kelvin) { return 1000000 / kelvin; }
 
-  sht.read();
-  float temperature = sht.getTemperature();
-  float humidite = sht.getHumidity();
-  float tempDiff = fabsf(temperature - derniereTemperature);
-  float humDiff = fabsf(humidite - derniereHumidite);
+uint16_t miredsToKelvin(uint16_t mireds) { return 1000000 / mireds; }
 
-  Serial.print("Temperature: ");
-  Serial.print(temperature, 1);
-  Serial.print(" °C\tHumidity: ");
-  Serial.print(humidite, 1);
-  Serial.println(" %");
-
-  pinMode(LED, OUTPUT);
-  digitalWrite(LED, LOW);
-
-  if (tempDiff > TEMP_THRESHOLD || humDiff > HUM_THRESHOLD) {
-
-    zbTempSensor.setManufacturerAndModel("Espressif", "Peter-ZigbeeTempSensor");
-    zbTempSensor.setMinMaxValue(-20, 50);
-    zbTempSensor.setTolerance(1);
-    zbTempSensor.addHumiditySensor(0, 100, 1, 0.0);
-
-    Zigbee.addEndpoint(&zbTempSensor);
-    Zigbee.setRxOnWhenIdle(false);
-
-    esp_zb_cfg_t zigbeeConfig = ZIGBEE_DEFAULT_ED_CONFIG();
-    zigbeeConfig.nwk_cfg.zed_cfg.keep_alive = 10000;
-    Zigbee.setTimeout(10000);
-
-    if (!Zigbee.begin(&zigbeeConfig, false)) {
-      Serial.println("Zigbee failed to start!");
-      Serial.println("Rebooting...");
-      ESP.restart();
-    }
-    Serial.println("Connecting to network");
-    while (!Zigbee.connected()) {
-      Serial.print(".");
-      delay(100);
-    }
-    Serial.println();
-    Serial.println("Successfully connected to Zigbee network");
-
-    zbTempSensor.setTemperature(temperature);
-    zbTempSensor.setHumidity(humidite);
-    zbTempSensor.report();
-    delay(250);
-
-    derniereTemperature = temperature;
-    derniereHumidite = humidite;
-    digitalWrite(LED, HIGH);
-  } else {
-    Serial.println("Pas d'envoi ZigBee :");
-    Serial.print("  Temp: ");
-    Serial.print(temperature, 1);
-    Serial.print(" °C (Δ ");
-    Serial.print(tempDiff, 2);
-    Serial.print(" °C ");
-    Serial.print(tempDiff > TEMP_THRESHOLD ? ">" : "<=");
-    Serial.print(" ");
-    Serial.print(TEMP_THRESHOLD, 1);
-    Serial.println(")");
-
-    Serial.print("  Hum:  ");
-    Serial.print(humidite, 1);
-    Serial.print(" % (Δ ");
-    Serial.print(humDiff, 2);
-    Serial.print(" % ");
-    Serial.print(humDiff > HUM_THRESHOLD ? ">" : "<=");
-    Serial.print(" ");
-    Serial.print(HUM_THRESHOLD, 1);
-    Serial.println(")");
+/********************* RGB LED functions **************************/
+void setRGBLight(bool state, uint8_t red, uint8_t green, uint8_t blue, uint8_t level) {
+  if (!state) {
+    rgbLedWrite(led, 0, 0, 0);
+    return;
   }
-
-  Serial.flush();
-  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-  esp_deep_sleep_start();
+  float brightness = (float)level / 255;
+  rgbLedWrite(led, red * brightness, green * brightness, blue * brightness);
 }
 
-void loop() {}
+/********************* Temperature LED functions **************************/
+void setTempLight(bool state, uint8_t level, uint16_t mireds) {
+  if (!state) {
+    rgbLedWrite(led, 0, 0, 0);
+    return;
+  }
+  float brightness = (float)level / 255;
+  // Convert mireds to color temperature (K) and map to white/yellow
+  uint16_t kelvin = miredsToKelvin(mireds);
+  uint8_t warm = constrain(map(kelvin, 2000, 6500, 255, 0), 0, 255);
+  uint8_t cold = constrain(map(kelvin, 2000, 6500, 0, 255), 0, 255);
+  rgbLedWrite(led, warm * brightness, warm * brightness, cold * brightness);
+}
+
+// Create a task on identify call to handle the identify function
+void identify(uint16_t time) {
+  static uint8_t blink = 1;
+  log_d("Identify called for %u seconds", time);
+  if (time == 0) {
+    // If identify time is 0, stop blinking and restore light as it was used for identify
+    zbColorLight.restoreLight();
+    return;
+  }
+  rgbLedWrite(led, 255 * blink, 255 * blink, 255 * blink);
+  blink = !blink;
+}
+
+/********************* Arduino functions **************************/
+void setup() {
+  Serial.begin(115200);
+
+  // Init RMT and leave light OFF
+  rgbLedWrite(led, 0, 0, 0);
+
+  // Init button for factory reset
+  pinMode(button, INPUT_PULLUP);
+
+  // Enable both XY (RGB) and Temperature color capabilities
+  uint16_t capabilities = ZIGBEE_COLOR_CAPABILITY_X_Y | ZIGBEE_COLOR_CAPABILITY_COLOR_TEMP;
+  zbColorLight.setLightColorCapabilities(capabilities);
+
+  // Set callback functions for RGB and Temperature modes
+  zbColorLight.onLightChangeRgb(setRGBLight);
+  zbColorLight.onLightChangeTemp(setTempLight);
+
+  // Optional: Set callback function for device identify
+  zbColorLight.onIdentify(identify);
+
+  // Optional: Set Zigbee device name and model
+  zbColorLight.setManufacturerAndModel("Espressif", "Peter-ZBColorLightBulb");
+
+  // Set min/max temperature range (High Kelvin -> Low Mireds: Min and Max is switched)
+  zbColorLight.setLightColorTemperatureRange(kelvinToMireds(6500), kelvinToMireds(2000));
+
+  // Add endpoint to Zigbee Core
+  Serial.println("Adding ZigbeeLight endpoint to Zigbee Core");
+  Zigbee.addEndpoint(&zbColorLight);
+
+  // When all EPs are registered, start Zigbee in End Device mode
+  if (!Zigbee.begin()) {
+    Serial.println("Zigbee failed to start!");
+    Serial.println("Rebooting...");
+    ESP.restart();
+  }
+  Serial.println("Connecting to network");
+  while (!Zigbee.connected()) {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println();
+}
+
+void loop() {
+  // Checking button for factory reset
+  if (digitalRead(button) == LOW) { // Push button pressed
+    // Key debounce handling
+    delay(100);
+    int startTime = millis();
+    while (digitalRead(button) == LOW) {
+      delay(50);
+      if ((millis() - startTime) > 3000) {
+        // If key pressed for more than 3secs, factory reset Zigbee and reboot
+        Serial.println("Resetting Zigbee to factory and rebooting in 1s.");
+        delay(1000);
+        Zigbee.factoryReset();
+      }
+    }
+    // Increase blightness by 50 every time the button is pressed
+    zbColorLight.setLightLevel(zbColorLight.getLightLevel() + 50);
+  }
+  delay(100);
+}
